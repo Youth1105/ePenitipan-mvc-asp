a9a3f7e314dd4a9af6734897ec7fbbc1a12bfc33
youthstorm <mbap.354@gmail.com>
2022-12-12 16:26:04 +0700

    Add project files.

---
+++ ePenitipan/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ePenitipan.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
